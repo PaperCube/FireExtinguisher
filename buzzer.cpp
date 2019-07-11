754fb8f6ab62641f4f13110308403933e1028b47
PaperCube <imzhy@hotmail.com>
2019-07-11 20:38:06 +0800

    Fix compilation error

--- buzzer.cpp
+++ buzzer.cpp
@@ -39,7 +39,7 @@ void buzzer::buzz(int timeout) {
 }
 
 void buzzer::buzz(buzz_pattern &pattern) {
-    prepare();
+    buzz_patterns::prepare();
     for (int i = 0; i < pattern.length; i++) {
         if ((i & 1) == 0) {
             buzz(pattern.pattern[i]);
